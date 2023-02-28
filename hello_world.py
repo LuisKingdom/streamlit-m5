f05dbefc88f4275a3aafc3dbde8940473707528b
LuisKingdom <luisavilamolina@outlook.com>
2023-02-28 15:47:06 -0600

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,15 +2,15 @@ import streamlit as st
 
 # Define the Streamlit app
 def app():
-    st.title("Wish List App")
-    wishes = st.text_area("Enter your wishes, one per line")
-    wishes = wishes.split("\n")
-    wishes = list(filter(None, wishes))
+    st.title("Requisitos")
+    requirements = st.text_area("Ingrese los requisitos, uno por línea")
+    requirements = requirements.split("\n")
+    requirements = list(filter(None, requirements))
     
-    if len(wishes) > 0:
-        st.write("Wishes:")
-        for i, wish in enumerate(wishes):
-            st.write(f"{i+1}. {wish}")
+    if len(requirements) > 0:
+        st.write("Requisitos:")
+        for i, requirement in enumerate(requirements):
+            st.write(f"{i+1}. {requirement}")
 
 if __name__ == '__main__':
     app()
