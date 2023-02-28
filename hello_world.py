38f8e30a1bb82c8f568906b5b49d59e5b20ccfdb
LuisKingdom <luisavilamolina@outlook.com>
2023-02-28 16:51:02 -0600

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,14 @@
 import streamlit as st
 from datetime import time
 
-st.header("Range Time Slider")
+st.header("Single Range Time Slider")
 
 # Default start and end time
 start_time = time(8, 0)
 end_time = time(18, 0)
 
-# User-selectable start and end time
-start_time = st.slider("Start Time", value=start_time, format="HH:mm")
-end_time = st.slider("End Time", value=end_time, format="HH:mm")
+# User-selectable time range
+selected_time_range = st.slider("Select Time Range", value=(start_time, end_time), format="HH:mm")
 
-# Display selected start and end time
-st.write("Selected Time Range: {} - {}".format(start_time.strftime("%H:%M"), end_time.strftime("%H:%M")))
+# Display selected time range
+st.write("Selected Time Range: {} - {}".format(selected_time_range[0].strftime("%H:%M"), selected_time_range[1].strftime("%H:%M")))
