1fba8cba9eab328aa2252f9db1b15845d2726c9d
LuisKingdom <luisavilamolina@outlook.com>
2023-02-28 15:26:18 -0600

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,22 @@
 import streamlit as st
 
-st.title('Adolfo Centeno Tellez')
-st.header('App demo de Streamlit')
-st.write('hola mundo de streamlit usando codespaces de github')
+# Create a list to hold the tasks
+tasks = []
+
+# Define a function to add tasks to the list
+def add_task(task):
+    tasks.append(task)
+
+# Define the Streamlit app
+def app():
+    st.title("To-Do List App")
+    task = st.text_input("Enter a task")
+    if st.button("Add Task"):
+        add_task(task)
+    if len(tasks) > 0:
+        st.write("Tasks:")
+        for i, task in enumerate(tasks):
+            st.write(f"{i+1}. {task}")
+
+if __name__ == '__main__':
+    app()
